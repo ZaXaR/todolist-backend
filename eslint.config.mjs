1da70d0259089a10db94dfabfc9b6ccc3191b488
ZaXaR <zaxar1990jem@gmail.com>
2025-07-25 15:53:00 +0300

    first init

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      'import/order': [
+        'warn',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'prettier/prettier': 'off',
+    },
+  },
+];
