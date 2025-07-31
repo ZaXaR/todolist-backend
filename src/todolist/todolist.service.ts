822666ca1f407ff86157d2ad03c76ff431ee8134
ZaXaR <zaxar1990jem@gmail.com>
2025-07-31 20:17:57 +0300

    warn not forgot

--- src/todolist/todolist.service.ts
+++ src/todolist/todolist.service.ts
@@ -1,81 +1,73 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
-import { TodoListDto, UpdateTodoListDto } from './todolist.dto';
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { TodoListDto, UpdateTodoListDto } from './todolist.dto'
 
 @Injectable()
 export class TodolistService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
+  // !need to rename Todo => Task to by smarter....
+  async getTodosListByUser(userId: string) {
+    const todos = await this.prisma.todoList.findMany({
+      where: { userId },
+      orderBy: { createdAt: 'desc' },
+    })
+    return todos
+  }
 
-    async getTodosListByUser(userId: string) {
-        const todos = await this.prisma.todoList.findMany({
-            where: { userId },
-            orderBy: { createdAt: 'desc' }
-        });
-        return todos;
+  async createTodo(userId: string, dto: TodoListDto) {
+    return this.prisma.todoList.create({
+      data: {
+        userId,
+        title: dto.title,
+        text: dto.text,
+        isCompleted: dto.isCompleted,
+        endDate: dto.endDate ? new Date(dto.endDate) : undefined,
+      },
+    })
+  }
+
+  async deleteTodoById(userId: string, todoId: string): Promise<void> {
+    const existingTask = await this.prisma.todoList.findFirst({
+      where: {
+        id: todoId,
+        userId: userId,
+      },
+    })
+
+    if (!existingTask) throw new BadRequestException({ message: 'Task not found or access denied' })
+
+    await this.prisma.todoList.delete({
+      where: { id: todoId },
+    })
+  }
+
+  async updateTodo(userId: string, todoId: string, dto: UpdateTodoListDto): Promise<TodoListDto> {
+    const existingTask = await this.prisma.todoList.findFirst({
+      where: {
+        id: todoId,
+        userId,
+      },
+    })
+
+    if (!existingTask) {
+      throw new BadRequestException('Task not found or access denied')
     }
 
-    async createTodo(userId: string, dto: TodoListDto) {
-        return this.prisma.todoList.create({
-            data: {
-                userId,
-                title: dto.title,
-                text: dto.text,
-                isCompleted: dto.isCompleted,
-                endDate: dto.endDate ? new Date(dto.endDate) : undefined,
-            },
-        });
+    const updatedTask = await this.prisma.todoList.update({
+      where: { id: todoId },
+      data: {
+        title: dto.title ?? existingTask.title,
+        text: dto.text ?? existingTask.text,
+        endDate: dto.endDate ?? existingTask.endDate,
+        isCompleted: dto.isCompleted ?? existingTask.isCompleted,
+      },
+    })
+
+    return {
+      title: updatedTask.title,
+      text: updatedTask.text,
+      isCompleted: updatedTask.isCompleted,
+      endDate: updatedTask.endDate ?? undefined,
     }
-
-    async deleteTodoById(userId: string, todoId: string): Promise<void> {
-        const existingTask = await this.prisma.todoList.findFirst({
-            where: {
-                id: todoId,
-                userId: userId,
-            },
-        });
-
-        if (!existingTask) throw new BadRequestException({ message: 'Task not found or access denied' });
-
-
-        await this.prisma.todoList.delete({
-            where: { id: todoId },
-        });
-    }
-
-
-    async updateTodo(
-        userId: string,
-        todoId: string,
-        dto: UpdateTodoListDto
-    ): Promise<TodoListDto> {
-        const existingTask = await this.prisma.todoList.findFirst({
-            where: {
-                id: todoId,
-                userId,
-            },
-        });
-
-        if (!existingTask) {
-            throw new BadRequestException('Task not found or access denied');
-        }
-
-        const updatedTask = await this.prisma.todoList.update({
-            where: { id: todoId },
-            data: {
-                title: dto.title ?? existingTask.title,
-                text: dto.text ?? existingTask.text,
-                endDate: dto.endDate ?? existingTask.endDate,
-                isCompleted: dto.isCompleted ?? existingTask.isCompleted,
-            },
-        });
-
-        return {
-            title: updatedTask.title,
-            text: updatedTask.text,
-            isCompleted: updatedTask.isCompleted,
-            endDate: updatedTask.endDate ?? undefined,
-        };
-
-    }
-
+  }
 }
