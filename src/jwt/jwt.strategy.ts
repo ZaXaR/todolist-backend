a4e84cc1153c8be9aff60a775067908891b71466
Дмитрий <zaxar1990jem@gmail.com>
2025-12-30 01:22:03 +0200

    some update

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -1,24 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { UserService } from "src/user/user.service";
 import { ConfigService } from "@nestjs/config";
 
+interface JwtPayload {
+  id: string;
+  email: string;
+}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        private configService: ConfigService,
-        private userService: UserService
-    ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: true,
-            secretOrKey: configService.get<string>('JWT_SECRET'),
-        });
-    }
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>("JWT_SECRET"),
+    });
+  }
 
-    async validate({ id }: { id: string }) {
-        return this.userService.getById(id);
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.getById(payload.id);
+    if (!user) {
+      throw new UnauthorizedException("User not found or invalid token");
     }
+    return user;
+  }
 }
