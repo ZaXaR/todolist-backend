07336f0c77ead7dca928efbf3a686130e55834c9
Дмитрий <zaxar1990jem@gmail.com>
2025-12-30 04:03:05 +0200

    add 3000 port

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
-  await app.listen(process.env.PORT ?? 4200);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
