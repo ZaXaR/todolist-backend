fb6c6ad536d23de48e3014cf6408c1a5b82249c2
Дмитрий <zaxar1990jem@gmail.com>
2025-12-30 02:19:37 +0200

    last update for hosting

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -1,32 +1,24 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { UserService } from "src/user/user.service";
 import { ConfigService } from "@nestjs/config";
 
-interface JwtPayload {
-  id: string;
-  email: string;
-}
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private configService: ConfigService,
-    private userService: UserService
-  ) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.get<string>("JWT_SECRET"),
-    });
-  }
-
-  async validate(payload: JwtPayload) {
-    const user = await this.userService.getById(payload.id);
-    if (!user) {
-      throw new UnauthorizedException("User not found or invalid token");
+    constructor(
+        private configService: ConfigService,
+        private userService: UserService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: true,
+            secretOrKey: configService.get<string>('JWT_SECRET'),
+        });
     }
-    return user;
-  }
+
+    async validate({ id }: { id: string }) {
+        return this.userService.getById(id);
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
